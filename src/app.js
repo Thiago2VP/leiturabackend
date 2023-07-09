86534f15ddc91aab78016cc305916a7f12db7c51
Thiago2VP <thiagovasconcelosvp@protonmail.com>
2023-07-09 16:49:48 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 
 import homeRoutes from './routes/home.js';
 import booksRoutes from './routes/books.js';
 
 dotenv.config();
+
+const whiteList = [process.env.FRONT_URL];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -14,6 +27,7 @@ class App {
   }
   
   middlewares() {
+    this.app.use(cors(corsOptions));
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
   }
